2882826dc815fba77bd76d262811cb22b469ef2b
Linus Groh <mail@linusgroh.de>
2023-10-26 17:05:16 +0100

    Replace static library with a module
    This allows importing the library in other projects.

--- build.zig
+++ build.zig
@@ -15,13 +15,8 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zigline",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("zigline", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
 
     const exe = b.addExecutable(.{
@@ -31,10 +26,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
     b.installArtifact(exe);
 
     // Creates a step for unit testing. This only builds the test executable
